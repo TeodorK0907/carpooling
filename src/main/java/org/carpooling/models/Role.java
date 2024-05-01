15cded6da9b5193a29da2cfcba95d01777b4022e
TeodorK0907 <ted_kehayov@abv.bg>
2024-05-01 13:23:34 +0300

    Develop Rating, Role, and User model

---
+++ src/main/java/org/carpooling/models/Role.java
@@ -0,0 +1,48 @@
+package org.carpooling.models;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private int id;
+    //todo make role names enums
+    @Column(name = "role_name")
+    private String name;
+
+    public Role() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role role)) return false;
+        return id == role.id && Objects.equals(name, role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
