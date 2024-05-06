e426f2b486240ec73fcacb622e9f9cae6dbbafbb
TeodorK0907 <ted_kehayov@abv.bg>
2024-05-06 21:55:30 +0300

    Update UserServiceImpl

--- src/main/java/org/carpooling/services/UserServiceImpl.java
+++ src/main/java/org/carpooling/services/UserServiceImpl.java
@@ -1,12 +1,22 @@
 package org.carpooling.services;
 
+import org.carpooling.exceptions.EntityNotFoundException;
 import org.carpooling.models.User;
+import org.carpooling.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public List<User> getAll() {
         return null;
@@ -14,11 +24,14 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User getById(int userId) {
-        return null;
+        return userRepository.findById(userId).orElseThrow(
+                () -> new EntityNotFoundException("user", "id", String.valueOf(userId)));
     }
 
     @Override
     public User create(User user) {
+
+        userRepository.save(user);
         return null;
     }
 
