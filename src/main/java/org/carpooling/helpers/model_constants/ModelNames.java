b67415c535b1625cc12cc30c31c2c2783f37f2ac
TeodorK0907 <ted_kehayov@abv.bg>
2024-05-08 15:57:02 +0300

    Develop ModelNames and UserAttribute enums

---
+++ src/main/java/org/carpooling/helpers/model_constants/ModelNames.java
@@ -0,0 +1,22 @@
+package org.carpooling.helpers.model_constants;
+
+public enum ModelNames {
+    USER("user"),
+    TRAVEL("travel"),
+    RATING("rating");
+
+    private final String description;
+
+    ModelNames(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public String toString() {
+        return this.description;
+    }
+}
