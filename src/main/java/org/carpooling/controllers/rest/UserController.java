9ac5aafe14a1fd962945acfa7768632dd5349fcc
TeodorK0907 <ted_kehayov@abv.bg>
2024-05-08 15:55:25 +0300

    Update UserController with exception handling

--- src/main/java/org/carpooling/controllers/rest/UserController.java
+++ src/main/java/org/carpooling/controllers/rest/UserController.java
@@ -1,13 +1,17 @@
 package org.carpooling.controllers.rest;
 
+import org.carpooling.exceptions.DuplicateEntityException;
+import org.carpooling.exceptions.EntityNotFoundException;
 import org.carpooling.mappers.UserMapper;
 import org.carpooling.models.User;
 import org.carpooling.models.input_dto.UpdateUserDto;
 import org.carpooling.models.input_dto.UserDto;
-import org.carpooling.services.UserService;
+import org.carpooling.services.contracts.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -27,27 +31,44 @@ public class UserController {
 
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
-        return ResponseEntity.ok().body(userService.getAll());
+        try {
+            List<User> result = userService.getAll();
+            return ResponseEntity.ok().body(result);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
     }
 
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody UserDto dto) {
-        User userToCreate = userMapper.toObj(dto);
-        userService.create(userToCreate);
-        return ResponseEntity.ok().body(userToCreate);
+        try {
+            User userToCreate = userMapper.toObj(dto);
+            userService.create(userToCreate);
+            return ResponseEntity.ok().body(userToCreate);
+        } catch (DuplicateEntityException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+        }
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<User> updateUser(@PathVariable int id,
                                            @RequestBody UpdateUserDto dto) {
-        User userToUpdate = userMapper.toObj(id, dto);
-        userService.update(userToUpdate);
-        return ResponseEntity.ok().body(userToUpdate);
+        try {
+            User userToUpdate = userMapper.toObj(id, dto);
+            userService.update(userToUpdate);
+            return ResponseEntity.ok().body(userToUpdate);
+        } catch (DuplicateEntityException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+        }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable int id) {
-        userService.delete(id);
-        return ResponseEntity.noContent().build();
+        try {
+            userService.delete(id);
+            return ResponseEntity.ok().build();
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
     }
 }
