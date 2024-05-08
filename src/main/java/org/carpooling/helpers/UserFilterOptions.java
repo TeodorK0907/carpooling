603d471a2b40c5232ae72bd989dd45b1b9aa44db
TeodorK0907 <ted_kehayov@abv.bg>
2024-05-08 21:57:12 +0300

    Develop UserFilterOptions and UserFilterValidator

---
+++ src/main/java/org/carpooling/helpers/UserFilterOptions.java
@@ -0,0 +1,41 @@
+package org.carpooling.helpers;
+
+import java.util.Optional;
+
+public class UserFilterOptions {
+    private Optional<String> username;
+    private Optional<String> email;
+    private Optional<String> phoneNumber;
+
+    public UserFilterOptions(String username,
+                                  String email,
+                                  String firstName) {
+        this.username = Optional.ofNullable(username);
+        this.email = Optional.ofNullable(email);
+        this.phoneNumber = Optional.ofNullable(firstName);
+    }
+
+    public Optional<String> getUsername() {
+        return username;
+    }
+
+    public void setUsername(Optional<String> username) {
+        this.username = username;
+    }
+
+    public Optional<String> getEmail() {
+        return email;
+    }
+
+    public void setEmail(Optional<String> email) {
+        this.email = email;
+    }
+
+    public Optional<String> getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Optional<String> phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ src/main/java/org/carpooling/helpers/model_validators/UserFilterValidator.java
@@ -0,0 +1,18 @@
+package org.carpooling.helpers.model_validators;
+
+import org.carpooling.helpers.UserFilterOptions;
+
+import java.util.Optional;
+
+public class UserFilterValidator {
+
+    public static String isFilterEmpty(Optional<String> filter) {
+        return filter.orElse(null);
+    }
+
+    public static Boolean isFilterEmpty(UserFilterOptions filter) {
+        return filter.getEmail().isEmpty()
+                && filter.getUsername().isEmpty()
+                && filter.getPhoneNumber().isEmpty();
+    }
+}
