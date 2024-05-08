c0174839730cb00acd028b91b452330a8c8cdc57
TeodorK0907 <ted_kehayov@abv.bg>
2024-05-08 15:56:17 +0300

    Update UserServiceImpl

--- src/main/java/org/carpooling/services/UserServiceImpl.java
+++ src/main/java/org/carpooling/services/UserServiceImpl.java
@@ -4,15 +4,19 @@ import org.carpooling.exceptions.EntityNotFoundException;
 import org.carpooling.helpers.UserValidator;
 import org.carpooling.models.User;
 import org.carpooling.repositories.UserRepository;
+import org.carpooling.services.contracts.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import static org.carpooling.helpers.model_constants.ModelNames.USER;
+import static org.carpooling.helpers.model_constants.attribute_constants.UserAttribute.ID;
 
 import java.util.List;
 import java.util.Optional;
 
+
 @Service
 public class UserServiceImpl implements UserService {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Autowired
     public UserServiceImpl(UserRepository userRepository) {
@@ -21,13 +25,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> getAll() {
-        return null;
+        List<User> users = userRepository.findAllByArchivedIsFalse();
+        UserValidator.validateIfUserListIsEmpty(users);
+        return users;
     }
 
     @Override
     public User getById(int userId) {
         return userRepository.findById(userId).orElseThrow(
-                () -> new EntityNotFoundException("user", "id", String.valueOf(userId)));
+                () -> new EntityNotFoundException(USER.toString(), ID.toString(), String.valueOf(userId)));
     }
 
     @Override
@@ -70,6 +76,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void delete(int userId) {
-
+        User toBeDeleted = getById(userId);
+        toBeDeleted.setArchived(true);
+        userRepository.save(toBeDeleted);
     }
 }
