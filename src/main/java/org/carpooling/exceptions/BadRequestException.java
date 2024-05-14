f2aa65a1133c00be3a7d4cd2fe76a645d3eaf229
TeodorK0907 <ted_kehayov@abv.bg>
2024-05-14 16:26:48 +0300

    Develop BadRequestException

---
+++ src/main/java/org/carpooling/exceptions/BadRequestException.java
@@ -0,0 +1,7 @@
+package org.carpooling.exceptions;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
