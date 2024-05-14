2beab0e3a700d0db240e91ce8aa4aa2a2d7bc6d8
TeodorK0907 <ted_kehayov@abv.bg>
2024-05-14 21:28:31 +0300

    Develop TravelValidator

---
+++ src/main/java/org/carpooling/helpers/validators/TravelValidator.java
@@ -0,0 +1,33 @@
+package org.carpooling.helpers.validators;
+
+import org.carpooling.exceptions.BadRequestException;
+import org.carpooling.exceptions.UnauthorizedOperationException;
+import org.carpooling.helpers.constants.TravelStatus;
+import org.carpooling.models.Travel;
+import org.carpooling.models.User;
+
+public class TravelValidator {
+    //todo remove below magic String
+    public static boolean isStatusActive(Travel travel) {
+        if (travel.getStatus() != TravelStatus.ACTIVE) {
+            throw new BadRequestException("Only a travel with an active status could be marked as completed.");
+        }
+        return true;
+    }
+
+    public static boolean isUserCreatorOfTravel(User authenticatedUser, User creator) {
+        if (!authenticatedUser.equals(creator)) {
+            throw new UnauthorizedOperationException("You are unauthorized to perform the required action.");
+        }
+        return true;
+    }
+
+    public static boolean isStatusCancelable(Travel toBeMarkedCancelled) {
+        switch (toBeMarkedCancelled.getStatus()) {
+            case ACTIVE, PLANNED:
+                return true;
+            default:
+                throw new BadRequestException("Only an active or planned travel could be cancelled.");
+        }
+    }
+}
