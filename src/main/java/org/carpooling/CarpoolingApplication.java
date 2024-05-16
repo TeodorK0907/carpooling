9096b0195bec1c307c742c5ae2bc0a4237589474
TeodorK0907 <ted_kehayov@abv.bg>
2024-05-16 22:11:04 +0300

    Add EnableConfigurationProperties to CarpoolingApplication

--- src/main/java/org/carpooling/CarpoolingApplication.java
+++ src/main/java/org/carpooling/CarpoolingApplication.java
@@ -1,9 +1,13 @@
 package org.carpooling;
 
+import org.carpooling.config.BingMapsPropertiesConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 
 @SpringBootApplication
+@EnableConfigurationProperties(BingMapsPropertiesConfig.class)
 public class CarpoolingApplication {
 
     public static void main(String[] args) {
