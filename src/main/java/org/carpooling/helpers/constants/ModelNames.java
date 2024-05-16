5e7ea1beec91adeb4429e3058ee031f3a7cca847
TeodorK0907 <ted_kehayov@abv.bg>
2024-05-16 22:11:14 +0300

    Update ModelNames

--- src/main/java/org/carpooling/helpers/constants/ModelNames.java
+++ src/main/java/org/carpooling/helpers/constants/ModelNames.java
@@ -3,6 +3,9 @@ package org.carpooling.helpers.constants;
 public enum ModelNames {
     USER("user"),
     TRAVEL("travel"),
+    TRAVEL_POINT("travel point"),
+    CANDIDATE("candidate"),
+    PASSENGER("passenger"),
     RATING("rating");
 
     private final String description;
