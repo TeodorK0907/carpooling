54c8476686b1f1a27edd68295907d17b13d4fcec
TeodorK0907 <ted_kehayov@abv.bg>
2024-05-17 18:11:41 +0300

    Update BingMapsClient, BingMapsClientImpl with getDistanceMatrix

--- src/main/java/org/carpooling/clients/BingMapsClientImpl.java
+++ src/main/java/org/carpooling/clients/BingMapsClientImpl.java
@@ -1,6 +1,10 @@
 package org.carpooling.clients;
 
 import org.carpooling.config.BingMapsWebClientConfig;
+import org.carpooling.helpers.constants.bing_maps_client.BingMapsClientEndpoint;
+import org.carpooling.helpers.constants.bing_maps_client.BingMapsClientKey;
+import org.carpooling.helpers.constants.bing_maps_client.BingMapsClientValue;
+import org.carpooling.models.TravelPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -18,10 +22,30 @@ public class BingMapsClientImpl implements BingMapsClient {
 
     @Override
     public String getLocationResponse(StringBuilder location) {
-        return client.create().get()
+        return client.create()
+                .get()
                 .uri(uriBuilder -> uriBuilder
                         .path(location.toString())
-                        .queryParam("key", key)
+                        .queryParam(BingMapsClientKey.KEY.toString(), key)
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+
+    @Override
+    public String getDistanceMatrixResponse(TravelPoint origins, TravelPoint destinations) {
+
+
+
+        return client.create()
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(BingMapsClientEndpoint.DISTANCE_MATRIX.toString())
+                        .queryParam(BingMapsClientKey.ORIGINS.toString(), originsLatLong.toString())
+                        .queryParam(BingMapsClientKey.DESTINATIONS.toString(), destinationsLatLong.toString())
+                        .queryParam(BingMapsClientKey.TRAVEL_MODE.toString(), BingMapsClientValue.DRIVING.toString())
+                        .queryParam(BingMapsClientKey.KEY.toString(), key)
                         .build())
                 .retrieve()
                 .bodyToMono(String.class)
