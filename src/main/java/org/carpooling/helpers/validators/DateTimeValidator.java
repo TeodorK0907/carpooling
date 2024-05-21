31f749788509369c7afec0b26a4220f573b3a8ad
TeodorK0907 <ted_kehayov@abv.bg>
2024-05-21 21:57:40 +0300

    update DateTimeMapper and DateTimeValidator

--- src/main/java/org/carpooling/helpers/validators/DateTimeValidator.java
+++ src/main/java/org/carpooling/helpers/validators/DateTimeValidator.java
@@ -3,19 +3,19 @@ package org.carpooling.helpers.validators;
 import org.carpooling.exceptions.BadRequestException;
 import org.carpooling.helpers.errors.TravelDtoErrors;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class DateTimeValidator {
-    private static final String DEFAULT_TIME_FORMAT = "yyyy-MM-dd HH:mm";
-    private static final SimpleDateFormat formatter = new SimpleDateFormat(DEFAULT_TIME_FORMAT);
+    private static final String DEFAULT_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm";
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern (DEFAULT_TIME_FORMAT);
 
     public static boolean isTimeFormatValid(String pattern) {
         try {
             formatter.parse(pattern);
             return true;
-        } catch (ParseException e) {
-            throw new BadRequestException(TravelDtoErrors.TIME);
+        } catch (DateTimeParseException e) {
+            throw new BadRequestException(TravelDtoErrors.DATE_TIME);
         }
     }
 }
