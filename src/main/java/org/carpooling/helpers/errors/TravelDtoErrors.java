38ab5838bf3824d538c1a8dcbad62c9b20100bd4
TeodorK0907 <ted_kehayov@abv.bg>
2024-05-21 21:59:15 +0300

    update TravelDtoErrors constants

--- src/main/java/org/carpooling/helpers/errors/TravelDtoErrors.java
+++ src/main/java/org/carpooling/helpers/errors/TravelDtoErrors.java
@@ -2,6 +2,6 @@ package org.carpooling.helpers.errors;
 
 public class TravelDtoErrors {
     public static final String FREE_SPOTS = "Free spots cannot be less than one.";
-    public static final String TIME = "The provided timeframe is invalid." +
-            "Please follow the format yyyy-MM-dd HH:mm.";
+    public static final String DATE_TIME = "A valid Date and Time that is in the present or future must be provided eg." +
+            "2024-07-30T15:10";
 }
