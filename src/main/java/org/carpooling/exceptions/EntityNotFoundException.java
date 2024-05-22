87214d43347555d34c07e4f9932bcb59d13da808
TeodorK0907 <ted_kehayov@abv.bg>
2024-05-22 21:51:48 +0300

    update EntityNotFoundException

--- src/main/java/org/carpooling/exceptions/EntityNotFoundException.java
+++ src/main/java/org/carpooling/exceptions/EntityNotFoundException.java
@@ -9,7 +9,14 @@ public class EntityNotFoundException extends RuntimeException{
         super(String.format("%s, with %s %s not found.", type, attribute, value));
     }
 
+    public EntityNotFoundException(String type, String attribute, String value,
+                                   String type2, String attribute2, String value2){
+        super(String.format("%s, with %s %s not found in %s with %s %s.",
+                type, attribute, value, type2, attribute2, value2));
+    }
+
     public EntityNotFoundException(String type, String attribute, String value1, String value2){
         super(String.format("%s, with %s %s does not have %s.", type, attribute, value1, value2));
     }
+
 }
