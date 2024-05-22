bc3a11d815957b9467803a05ace475fb5ac67a8a
TeodorK0907 <ted_kehayov@abv.bg>
2024-05-22 21:50:51 +0300

    update CommentRepository and CommentServiceImpl

--- src/main/java/org/carpooling/repositories/CommentRepository.java
+++ src/main/java/org/carpooling/repositories/CommentRepository.java
@@ -8,6 +8,5 @@ import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
-    @Query("select c from Comment c where c.travel_id = :travelId")
     Optional<Comment> findByTravelId(int travelId);
 }
