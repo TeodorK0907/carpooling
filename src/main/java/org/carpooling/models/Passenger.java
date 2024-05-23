971a7aec8409b895604cc6185bdb58157cfe0720
TeodorK0907 <ted_kehayov@abv.bg>
2024-05-24 00:20:45 +0300

    update Passenger and Candidate class with inheritance

--- src/main/java/org/carpooling/models/Passenger.java
+++ src/main/java/org/carpooling/models/Passenger.java
@@ -1,51 +1,31 @@
 package org.carpooling.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 import java.util.Objects;
+
 @Entity
 @Table(name = "passengers", schema = "rose-valley-travel")
-public class Passenger {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "passenger_id")
-    private int id;
-    @JsonIgnore
-    @Column(name = "user_id")
-    private int userId;
-    @Column(name = "username")
-    private String username;
+public class Passenger extends Candidate {
+    //    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "passenger_id")
+//    private int id;
+//    @JsonIgnore
+//    @Column(name = "user_id")
+//    private int userId;
+//    @Column(name = "travel_id")
+//    private int travelId;
+//    @Column(name = "username")
+//    private String username;
     @Column(name = "gave_rating")
     private boolean gaveRating;
     @Column(name = "given_rating")
     private boolean givenRating;
 
     public Passenger() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 
     public boolean isGaveRating() {
@@ -68,15 +48,12 @@ public class Passenger {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Passenger passenger)) return false;
-        return id == passenger.id
-                && userId == passenger.userId
-                && gaveRating == passenger.gaveRating
-                && givenRating == passenger.givenRating
-                && Objects.equals(username, passenger.username);
+        return gaveRating == passenger.gaveRating
+                && givenRating == passenger.givenRating;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, userId, username, gaveRating, givenRating);
+        return Objects.hash(gaveRating, givenRating);
     }
 }
