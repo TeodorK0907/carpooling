5640fea6954642217a8926cc228d45794f1f5000
TeodorK0907 <ted_kehayov@abv.bg>
2024-05-24 00:21:24 +0300

    update CandidateService, CandidateServiceImpl and CandidateRepository

--- src/main/java/org/carpooling/services/CandidateServiceImpl.java
+++ src/main/java/org/carpooling/services/CandidateServiceImpl.java
@@ -3,42 +3,72 @@ package org.carpooling.services;
 import org.carpooling.exceptions.EntityNotFoundException;
 import org.carpooling.helpers.constants.ModelNames;
 import org.carpooling.helpers.constants.attribute_constants.CandidateAttributes;
+import org.carpooling.helpers.validators.UserValidator;
 import org.carpooling.models.Candidate;
+import org.carpooling.models.Travel;
 import org.carpooling.models.User;
 import org.carpooling.repositories.CandidateRepository;
 import org.carpooling.services.contracts.CandidateService;
+import org.carpooling.services.contracts.TravelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CandidateServiceImpl implements CandidateService {
     private final CandidateRepository candidateRepository;
+    private final TravelService travelService;
 
     @Autowired
-    public CandidateServiceImpl(CandidateRepository candidateRepository) {
+    public CandidateServiceImpl(CandidateRepository candidateRepository,
+                                TravelService travelService) {
         this.candidateRepository = candidateRepository;
+        this.travelService = travelService;
     }
 
     @Override
-    public Candidate create(User user) {
-        Candidate candidate = new Candidate();
-        candidate.setUserId(user.getId());
-        candidate.setUsername(user.getUsername());
+    public Candidate apply(User user, Candidate candidate, int travelId) {
+        UserValidator.isBlocked(user);
+        candidateRepository.save(candidate);
+        Travel travelToApply = travelService.getById(travelId);
+
+        travelToApply.getCandidates().add(candidate);
+        travelService.update(travelToApply);
         return candidate;
     }
 
     @Override
-    public Candidate getByUserId(int userId) {
-       return candidateRepository.findByUserId(userId)
-               .orElseThrow(() -> new EntityNotFoundException(
-                       ModelNames.CANDIDATE.toString(),
-                       CandidateAttributes.USER_ID.toString(),
-                       String.valueOf(userId)
-               ));
+    public Candidate getById(int candidateId) {
+        return candidateRepository.findById(candidateId)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        ModelNames.CANDIDATE.toString(),
+                        CandidateAttributes.ID.toString(),
+                        String.valueOf(candidateId)
+                ));
     }
 
     @Override
-    public void delete(Candidate candidate) {
+    public Candidate getByUserIdAndTravelId(int userId, int travelId) {
+        return candidateRepository.findByUserIdAndTravelId(userId, travelId)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        ModelNames.CANDIDATE.toString(),
+                        CandidateAttributes.USER_ID.toString(),
+                        String.valueOf(userId)
+                ));
+    }
 
+    @Override
+    public void resign(User user, int travelId) {
+        //           if (CandidateValidator.isCandidaterInTravel(travelToApply, candidate)) {
+//               throw new DuplicateEntityException(
+//                       ModelNames.CANDIDATE.toString(),
+//                       CandidateAttributes.USER_ID.toString(),
+//                       String.valueOf(candidate.getUserId())
+//               );
+//           }
+        Candidate candidate = getByUserIdAndTravelId(user.getId(), travelId);
+        Travel travel = travelService.getById(travelId);
+        travel.getCandidates().remove(candidate);
+        travelService.update(travel);
+        candidateRepository.delete(candidate);
     }
 }
