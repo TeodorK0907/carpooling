9adef57b71ca4356a5075f387e822397f4ae113e
TeodorK0907 <ted_kehayov@abv.bg>
2024-05-24 15:40:12 +0300

    fix Feedbacks model typo

--- src/main/java/org/carpooling/models/Rating.java
+++ src/main/java/org/carpooling/models/Rating.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table(name = "Feedbacks", schema = "rose-valley-travel")
+@Table(name = "feedbacks", schema = "rose-valley-travel")
 public class Rating {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
