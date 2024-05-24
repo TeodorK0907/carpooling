3e6657deb708f6a33234e4709e2480f5846e6f59
TeodorK0907 <ted_kehayov@abv.bg>
2024-05-24 15:39:48 +0300

    update PassengerController and PassengerServiceImpl

--- src/main/java/org/carpooling/services/PassengerServiceImpl.java
+++ src/main/java/org/carpooling/services/PassengerServiceImpl.java
@@ -5,10 +5,12 @@ import org.carpooling.helpers.constants.ModelNames;
 import org.carpooling.helpers.constants.attribute_constants.CandidateAttributes;
 import org.carpooling.helpers.constants.attribute_constants.PassengerAttributes;
 import org.carpooling.helpers.validators.TravelValidator;
+import org.carpooling.models.Candidate;
 import org.carpooling.models.Passenger;
 import org.carpooling.models.Travel;
 import org.carpooling.models.User;
 import org.carpooling.repositories.PassengerRepository;
+import org.carpooling.services.contracts.CandidateService;
 import org.carpooling.services.contracts.PassengerService;
 import org.carpooling.services.contracts.TravelService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,19 +20,28 @@ import org.springframework.stereotype.Service;
 public class PassengerServiceImpl implements PassengerService {
     private final PassengerRepository passengerRepository;
     private final TravelService travelService;
+    private final CandidateService candidateService;
 
     @Autowired
     public PassengerServiceImpl(PassengerRepository passengerRepository,
-                                TravelService travelService) {
+                                TravelService travelService,
+                                CandidateService candidateService) {
         this.passengerRepository = passengerRepository;
         this.travelService = travelService;
+        this.candidateService = candidateService;
     }
 
     @Override
     public Passenger approve(User user, Passenger passenger, int travelId) {
         Travel travel = travelService.getById(travelId);
         TravelValidator.isUserCreatorOfTravel(user, travel.getCreator());
-        //todo finish below method body
+        Candidate toRemove = candidateService.getByUserIdAndTravelId(
+                passenger.getUserId(), travelId);
+        travel.getCandidates().remove(toRemove);
+        candidateService.delete(toRemove);
+        passengerRepository.save(passenger);
+        travel.getPassengers().add(passenger);
+        travelService.update(travel);
         return passenger;
     }
 
@@ -59,8 +70,8 @@ public class PassengerServiceImpl implements PassengerService {
         Travel travel = travelService.getById(travelId);
         TravelValidator.isUserCreatorOfTravel(user, travel.getCreator());
         Passenger toRemove = getById(passengerId);
-        //todo finish rest of method
-       // travel.getPassengers().remove(pas)
-
+        travel.getPassengers().remove(toRemove);
+        travelService.update(travel);
+        passengerRepository.delete(toRemove);
     }
 }
