cc2b2f8589e89d0a6ec855a54728601bec3f16a2
TeodorK0907 <ted_kehayov@abv.bg>
2024-05-29 16:36:26 +0300

    Update UserFilterValidator and TravelFilterValidator with sorting, paging and order validations

--- src/main/java/org/carpooling/helpers/validators/UserFilterValidator.java
+++ src/main/java/org/carpooling/helpers/validators/UserFilterValidator.java
@@ -1,14 +1,19 @@
 package org.carpooling.helpers.validators;
 
+import org.carpooling.helpers.constants.attribute_constants.UserAttribute;
 import org.carpooling.helpers.model_filters.UserFilterOptions;
+import org.springframework.data.domain.Sort;
 
 import java.util.Optional;
-
 
 public class UserFilterValidator {
 
     private static final int DEFAULT_PAGE = 0;
     private static final int DEFAULT_PAGE_SIZE = 10;
+    private static final String USERNAME = "username";
+    private static final String EMAIL = "email";
+    private static final String PHONE_NUMBER = "phoneNumber";
+    private static final String DESC = "desc";
 
     public static int getPageNum(Optional<Integer> pageNum) {
         if (pageNum.isEmpty()) {
@@ -22,6 +27,39 @@ public class UserFilterValidator {
             return DEFAULT_PAGE_SIZE;
         }
         return pageSize.get();
+    }
+
+    public static String getSort(Optional<String> sortBy) {
+        String sort;
+        if (sortBy.isEmpty()) {
+            return UserAttribute.ID.toString();
+        }
+
+        switch (sortBy.get()) {
+            case USERNAME:
+                sort = USERNAME;
+                break;
+            case EMAIL:
+                sort = EMAIL;
+                break;
+            case PHONE_NUMBER:
+                sort = PHONE_NUMBER;
+                break;
+            default:
+                sort = UserAttribute.ID.toString();
+        }
+        return sort;
+    }
+
+    public static Sort.Direction getOrder(Optional<String> orderBy) {
+        if (orderBy.isEmpty()) {
+            return Sort.Direction.ASC;
+        }
+        if (orderBy.get().equalsIgnoreCase(DESC)) {
+            return Sort.Direction.DESC;
+        } else {
+            return Sort.Direction.ASC;
+        }
     }
 
     public static Boolean isFilterEmpty(UserFilterOptions filter) {
