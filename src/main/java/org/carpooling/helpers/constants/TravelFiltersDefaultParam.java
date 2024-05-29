7a910d564b3ec7cb38a8b97b9028cbdb4606929a
TeodorK0907 <ted_kehayov@abv.bg>
2024-05-29 16:36:01 +0300

    Develop UserFiltersDefaultParam and rename TravelFilters to TravelFiltersDefaultParam with new constants

--- src/main/java/org/carpooling/helpers/constants/TravelFiltersDefaultParam.java
+++ src/main/java/org/carpooling/helpers/constants/TravelFiltersDefaultParam.java
@@ -2,8 +2,9 @@ package org.carpooling.helpers.constants;
 
 import java.time.LocalDateTime;
 
-public class TravelFilters {
+public class TravelFiltersDefaultParam {
     public static final String EMPTY_STRING_FILTER = "";
+    public static final String FORMAT = "%%%s%%";
     public static final LocalDateTime EMPTY_DATE_AFTER = LocalDateTime.of(
             1900, 1, 1, 0, 0);
     public static final LocalDateTime EMPTY_DATE_BEFORE = LocalDateTime.of(
