85eb1e69ca63f864358ce7ce6e2581380d3f0cb7
TeodorK0907 <ted_kehayov@abv.bg>
2024-05-30 22:56:28 +0300

    Update CarpoolingApplication with MailJetPropertiesConfig properties support

--- src/main/java/org/carpooling/CarpoolingApplication.java
+++ src/main/java/org/carpooling/CarpoolingApplication.java
@@ -1,13 +1,15 @@
 package org.carpooling;
 
-import org.carpooling.config.BingMapsPropertiesConfig;
+import org.carpooling.config.bing_maps.BingMapsPropertiesConfig;
+import org.carpooling.config.mail_jet.MailJetPropertiesConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
 @SpringBootApplication
-@EnableConfigurationProperties(BingMapsPropertiesConfig.class)
+@EnableConfigurationProperties(
+        {BingMapsPropertiesConfig.class, MailJetPropertiesConfig.class})
 public class CarpoolingApplication {
 
     public static void main(String[] args) {
