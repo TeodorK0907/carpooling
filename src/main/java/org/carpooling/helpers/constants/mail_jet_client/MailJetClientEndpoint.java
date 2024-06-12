e9239d910bdc424b021957d81be62b7c5b48032f
TeodorK0907 <ted_kehayov@abv.bg>
2024-06-12 23:56:40 +0300

    update MailJetClientEndpoint enum

--- src/main/java/org/carpooling/helpers/constants/mail_jet_client/MailJetClientEndpoint.java
+++ src/main/java/org/carpooling/helpers/constants/mail_jet_client/MailJetClientEndpoint.java
@@ -1,6 +1,8 @@
 package org.carpooling.helpers.constants.mail_jet_client;
 public enum MailJetClientEndpoint {
     SEND("/send"),
+    V3PointOne("/v3.1"),
+    V3("/v3/"),
     MESSAGE("/message");
 
     private final String description;
