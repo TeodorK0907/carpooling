c2587cbf3d434a0ef25c333db3f311a22927431c
TeodorK0907 <ted_kehayov@abv.bg>
2024-06-13 19:16:45 +0300

    update MailJetResponseHandler's Json response iteration

--- src/main/java/org/carpooling/helpers/handlers/MailJetResponseHandler.java
+++ src/main/java/org/carpooling/helpers/handlers/MailJetResponseHandler.java
@@ -1,26 +1,32 @@
 package org.carpooling.helpers.handlers;
 
 import org.carpooling.exceptions.UnsuccessfulResponseException;
+import org.carpooling.helpers.constants.mail_jet_client.MailJetClientKey;
 import org.carpooling.helpers.errors.MailJetClientErrors;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class MailJetResponseHandler {
-
-    public static String extractMailId(String response) {
+    private static final int FIRST_INDEX = 0;
+    public static Long extractMailId(String response) {
         JSONObject responseJson = new JSONObject(response);
-        JSONArray messages = responseJson.getJSONArray("Messages");
-        if (messages.getString(0).equals("Success")) {
-            JSONArray to = messages.getJSONArray(1);
-            JSONObject messageId = to.getJSONObject(1);
-            return messageId.getString("MessageId");
+        JSONArray messages = responseJson.getJSONArray(MailJetClientKey.MESSAGES.toString());
+        JSONObject messagesContent = messages.getJSONObject(FIRST_INDEX);
+        if (messagesContent.getString(
+                MailJetClientKey.STATUS.toString()).equals(MailJetClientKey.SUCCESS.toString()))
+        {
+            JSONArray to = messagesContent.getJSONArray(MailJetClientKey.TO.toString());
+            JSONObject toContent = to.getJSONObject(FIRST_INDEX);
+            return toContent.getLong(MailJetClientKey.MESSAGE_ID.toString());
+
         }
         throw new UnsuccessfulResponseException(MailJetClientErrors.FAILED_RESPONSE.toString());
     }
 
     public static String extractStatus(String response) {
         JSONObject responseJson = new JSONObject(response);
-        JSONArray dataArr = responseJson.getJSONArray("Data");
-        return dataArr.getString(9);
+        JSONArray dataArr = responseJson.getJSONArray(MailJetClientKey.DATA.toString());
+        JSONObject data = dataArr.getJSONObject(FIRST_INDEX);
+        return data.getString(MailJetClientKey.STATUS.toString());
     }
 }
